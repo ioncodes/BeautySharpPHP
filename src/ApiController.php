7bfacf0a8acfaa8dbb580b18bfdc8f312e7a662c
ion <ionix@protonmail.com>
2016-07-21 12:53:14 +0200

    Initial commit.

---
+++ src/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luca
+ * Date: 21.07.2016
+ * Time: 12:36
+ */
+
+namespace BeautySharpPHP;
+
+
+class ApiController
+{
+    public function __construct()
+    {
+        // bla
+    }
+}
