28d76355be126aa94509751cf393dd66d1b23acc
ion <ionix@protonmail.com>
2016-07-25 14:13:04 +0200

    change conn to PDO

--- src/api.php
+++ src/api.php
@@ -5,11 +5,15 @@ $servername = "localhost";
 $username = "bsharpapi";
 $password = "tTLtHXG232FHvLTK";
 
-$conn = new mysqli($servername, $username, $password);
-
-if ($conn->connect_error) {
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=beautysharp", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
     die("Server down"); // srsly?
 }
+
 
 // let's have some fun...
 
@@ -18,7 +22,7 @@ if(isset($_POST["addsource"]) && isset($_POST["token"])) { // everything fine
     $token = $_POST{'token'};
     $source = "Console.Write('Hello World');"; // testing, dev...
     echo $source;
-    
+
     exit();
 }
 else
