6f5d2b170a22921bdc5816c3440f03db7c6ec9ad
Luca <ionix@protonmail.com>
2016-07-25 13:31:40 +0200

    Upload new file

---
+++ src/api.php
@@ -0,0 +1,15 @@
+<script src="js/highlight.pack.js" type="application/javascript" ></script>
+
+<?php
+if(isset($_POST["addsource"]) && isset($_POST["token"])) { // everything fine
+    $source = $_POST['addsource'];
+    $token = $_POST{'token'};
+    $source = "Console.Write('Hello World');"; // testing, dev
+    echo $source;
+    
+    exit();
+}
+else
+{
+    die("Unauthorized"); // Fuck you!
+}
