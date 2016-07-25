c93c44d0eb3e7faaccad91aafe03a69f9aafec7f
ion <ionix@protonmail.com>
2016-07-25 14:06:56 +0200

    added DB connection

--- src/api.php
+++ src/api.php
@@ -1,6 +1,18 @@
 <script src="js/highlight.pack.js" type="application/javascript" ></script>
 
 <?php
+$servername = "localhost";
+$username = "bsharpapi";
+$password = "tTLtHXG232FHvLTK";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Server down"); // srsly?
+}
+
+// let's have some fun...
+
 if(isset($_POST["addsource"]) && isset($_POST["token"])) { // everything fine
     $source = $_POST['addsource'];
     $token = $_POST{'token'};
