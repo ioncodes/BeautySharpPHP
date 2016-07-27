640cd4771e4740897dc8cb09428ad1a2df5c1bd9
Elvio Petillo <elvio.petillo@nanowaris.com>
2016-07-27 16:22:45 +0200

    push, push, push

--- src/index.php
+++ src/index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="style/dracula.css">
+
 <?php
 
 $id = $_REQUEST["paste"];
@@ -293,99 +295,9 @@ namespace JodelBooster
 }
 ";
 
-$colored_words_array = [
-    'blue' => [
-        "abstract",
-        "as",
-        "base",
-        "bool",
-        "break",
-        "byte",
-        "case",
-        "catch",
-        "char",
-        "checked",
-        "class",
-        "const",
-        "continue",
-        "decimal",
-        "default",
-        "delegate",
-        "do",
-        "double",
-        "else",
-        "enum",
-        "event",
-        "explicit",
-        "extern",
-        "false",
-        "finally",
-        "fixed",
-        "float",
-        "for",
-        "foreach",
-        "goto",
-        "if",
-        "implicit",
-        "in",
-        "int",
-        "interface",
-        "internal",
-        "is",
-        "lock",
-        "long",
-        "namespace",
-        "new",
-        "null",
-        "object",
-        "operator",
-        "out",
-        "override",
-        "params",
-        "private",
-        "protected",
-        "public",
-        "readonly",
-        "ref",
-        "return",
-        "sbyte",
-        "sealed",
-        "short",
-        "sizeof",
-        "stackalloc",
-        "static",
-        "string",
-        "struct",
-        "switch",
-        "this",
-        "throw",
-        "true",
-        "try",
-        "typeof",
-        "uint",
-        "ulong",
-        "unchecked",
-        "unsafe",
-        "ushort",
-        "using",
-        "virtual",
-        "volatile",
-        "void",
-        "while"
-    ],
-    'yellow' => [
-
-    ]
-];
-
 $colored_content = $content;
 
-foreach ($colored_words_array as $color_name => $word_array) {
-    foreach ($word_array as $word) {
-        $colored_span = '<span style="color:'.$color_name.'">'.$word.'</span>'; //TODO: replace $color_name by $hex_color!
-        $colored_content = preg_replace('/\b'.$word.'\b/', $colored_span, $content);
-        $content = $colored_content;
-    }
-}
-
-echo '<pre>'.$colored_content.'</pre>';
+echo '<pre><code class="cs">'.$colored_content.'</code></pre>';
+?>
+<script type="application/javascript" src="js/highlight.pack.js" ></script>
+<script>hljs.initHighlightingOnLoad();</script>
