719560c715b95b42558504756333ee27eab1a904
Elvio Petillo <elvio.petillo@nanowaris.com>
2016-07-27 15:58:26 +0200

    Just realized there's highlight.js...

--- src/index.php
+++ src/index.php
@@ -295,7 +295,86 @@ namespace JodelBooster
 
 $colored_words_array = [
     'blue' => [
-        "string", "using", "const", "bool", "int", "for", "try", "new", "return", "void"
+        "abstract",
+        "as",
+        "base",
+        "bool",
+        "break",
+        "byte",
+        "case",
+        "catch",
+        "char",
+        "checked",
+        "class",
+        "const",
+        "continue",
+        "decimal",
+        "default",
+        "delegate",
+        "do",
+        "double",
+        "else",
+        "enum",
+        "event",
+        "explicit",
+        "extern",
+        "false",
+        "finally",
+        "fixed",
+        "float",
+        "for",
+        "foreach",
+        "goto",
+        "if",
+        "implicit",
+        "in",
+        "int",
+        "interface",
+        "internal",
+        "is",
+        "lock",
+        "long",
+        "namespace",
+        "new",
+        "null",
+        "object",
+        "operator",
+        "out",
+        "override",
+        "params",
+        "private",
+        "protected",
+        "public",
+        "readonly",
+        "ref",
+        "return",
+        "sbyte",
+        "sealed",
+        "short",
+        "sizeof",
+        "stackalloc",
+        "static",
+        "string",
+        "struct",
+        "switch",
+        "this",
+        "throw",
+        "true",
+        "try",
+        "typeof",
+        "uint",
+        "ulong",
+        "unchecked",
+        "unsafe",
+        "ushort",
+        "using",
+        "virtual",
+        "volatile",
+        "void",
+        "while"
+    ],
+    'yellow' => [
+
     ]
 ];
 
@@ -304,7 +383,7 @@ $colored_content = $content;
 foreach ($colored_words_array as $color_name => $word_array) {
     foreach ($word_array as $word) {
         $colored_span = '<span style="color:'.$color_name.'">'.$word.'</span>'; //TODO: replace $color_name by $hex_color!
-        $colored_content = str_replace($word, $colored_span, $content);
+        $colored_content = preg_replace('/\b'.$word.'\b/', $colored_span, $content);
         $content = $colored_content;
     }
 }
